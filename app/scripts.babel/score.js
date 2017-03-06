b39e9322f5e214fd635026c20290534772a8997d
Nikolas Bravo <nikolasbra@gmail.com>
2017-03-06 12:00:51 -0300

    refactoring

--- app/scripts.babel/score.js
+++ app/scripts.babel/score.js
@@ -1,11 +1,9 @@
 'use strict';
 
-start();
-
 /**
  * Starts the score calculation
  */
-function start() {
+function onLoad() {
   let score = getScore();
   writeScore(score);
 }
@@ -21,18 +19,35 @@ function getScore() {
 }
 
 /**
+ * Verifies if the repository is public or private
+ * @return {boolean} true if it is public, else false
+ */
+function repositoryIsPublic() {
+  // TODO should find a better way to verify. Maybe using the GitHub API(?)
+  if(document.getElementsByClassName('public')[0] !== undefined) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Gets the node subtree where the score box will be inserted at
+ * @return {object} the node subtree
+ */
+ function getSubtreeToInsert() {
+  if (repositoryIsPublic()) {
+    return(document.getElementsByClassName('public')[0]);
+  }
+  return(document.getElementsByClassName('private')[0]);
+}
+
+/**
  * Writes the score block to the DOM
  * @param {object} score The score values
  */
 function writeScore(score) {
   let nodeToInsert = getNodeToInsert(score);
-
-  // TODO should find a better way to verify. Maybe using the GitHub API(?)
-  let repoTopName = document.getElementsByClassName('public')[0];
-  if(repoTopName === undefined) {
-    repoTopName = document.getElementsByClassName('private')[0];
-  }
-
+  let repoTopName = getSubtreeToInsert();
   repoTopName.insertBefore(nodeToInsert, repoTopName.firstChild);
 }
 
@@ -45,3 +60,5 @@ function getNodeToInsert(score) {
   let node = document.createTextNode(score.aggregate + '');
   return node;
 }
+
+window.onhashchange = onLoad();
