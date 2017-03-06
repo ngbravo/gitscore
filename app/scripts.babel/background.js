00b7410370d1145a0c15a44db83bf0243f222cff
Nikolas Bravo <ngbravo@users.noreply.github.com>
2017-03-06 11:13:07 -0300

    Revert "Defined eslint styles"

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,10 +1,10 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener((details) => {
+chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.tabs.onUpdated.addListener((tabId) => {
+chrome.tabs.onUpdated.addListener(tabId => {
   chrome.pageAction.show(tabId);
 });
 
--- app/scripts.babel/score.js
+++ app/scripts.babel/score.js
@@ -2,46 +2,32 @@
 
 start();
 
-/**
- * Starts the score calculation
- */
-function start() {
-  let score = getScore();
+function start(){
+
+  var score = getScore();
   writeScore(score);
 }
 
-/**
- * Gets the repository score
- * @return {object} The score information
- */
-function getScore() {
+function getScore(){
   return {
-    aggregate: 2,
+    'aggregate': 2
   };
 }
 
-/**
- * Writes the score to the DOM
- * @param {Number} score The score value
- */
-function writeScore(score) {
-  let nodeToInsert = getNodeToInsert(score);
+function writeScore(score){
+  var nodeToInsert = getNodeToInsert(score);
 
-  // TODO should find a better way to verify. Maybe using the GitHub API(?)
-  let repoTopName = document.getElementsByClassName('public')[0];
+  //TODO should find a better way to verify. Maybe using the GitHub API(?)
+  var repoTopName = document.getElementsByClassName('public')[0];
   if(repoTopName === undefined) {
     repoTopName = document.getElementsByClassName('private')[0];
   }
 
+  var childNodes = repoTopName.childNodes;
   repoTopName.insertBefore(nodeToInsert, repoTopName.firstChild);
 }
 
-/**
- * Generates the DOM node that will be inserted in the document
- * @param {Number} score The score value
- * @return {Text}
- */
 function getNodeToInsert(score) {
-  let node = document.createTextNode(score.aggregate + '');
+  var node = document.createTextNode(score.aggregate + '')
   return node;
 }
